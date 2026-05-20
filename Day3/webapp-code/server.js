417231fffb8734bf11c37c99ab7fb58b853945ac
Sanika <sanikakhire891@gmail.com>
2026-05-20 15:02:40 +0530

    Added Index.html file

--- Day3/webapp-code/server.js
+++ Day3/webapp-code/server.js
@@ -1,13 +1,15 @@
-const express = require("express");
-
+const express = require('express');
+const path = require('path');
+ 
 const app = express();
-
-const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-    res.send("Hello World from Azure Web App!");
+const port = process.env.PORT || 8080;
+ 
+app.use(express.static(__dirname));
+ 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+ 
+app.listen(port, () => {
+  console.log(`Web app running on port ${port}`);
 });
